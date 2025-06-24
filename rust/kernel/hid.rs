4b7fee432e8b1028d7359963d215798408e394c2
Rahul Rameshbabu <sergeantsagara@protonmail.com>
2025-06-23 18:27:04 -0700

    Make a reference to the underlying hdrv value in the hid register path

--- rust/kernel/hid.rs
+++ rust/kernel/hid.rs
@@ -162,15 +162,15 @@ unsafe impl<T: Driver + 'static> driver::RegistrationOps for Adapter<T> {
         name: &'static CStr,
         module: &'static ThisModule,
     ) -> Result {
-        unsafe {
-            (*hdrv.get()).name = name.as_char_ptr();
-            (*hdrv.get()).id_table = T::ID_TABLE.as_ptr();
-            (*hdrv.get()).report_fixup = if T::HAS_REPORT_FIXUP {
-                Some(Self::report_fixup_callback)
-            } else {
-                None
-            };
-        }
+        let raw_hdrv = unsafe { &mut *hdrv.get() };
+
+        raw_hdrv.name = name.as_char_ptr();
+        raw_hdrv.id_table = T::ID_TABLE.as_ptr();
+        raw_hdrv.report_fixup = if T::HAS_REPORT_FIXUP {
+            Some(Self::report_fixup_callback)
+        } else {
+            None
+        };
 
         to_result(unsafe {
             bindings::__hid_register_driver(hdrv.get(), module.0, name.as_char_ptr())
